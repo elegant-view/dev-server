3237dd67c958a2e007fa15687f28a88de576f204
yibuyisheng <yibuyisheng@163.com>
2016-04-10 12:36:31 +0800

    path

--- src/handlers/jasmine.js
+++ src/handlers/jasmine.js
@@ -5,9 +5,10 @@
 
 import path from 'path';
 import * as util from '../util';
+import winston from 'winston';
 
 export default function getHandler() {
-    let jasmineDir = path.resolve('../../node_modules/jasmine-core/lib/jasmine-core/');
+    let jasmineDir = path.join(process.cwd(), './node_modules/jasmine-core/lib/jasmine-core/');
     return function handler(context) {
         let urlPath = context.getUrlPath();
         let fileReading;
@@ -26,14 +27,16 @@ export default function getHandler() {
                     contents.push(content);
                     context.js(contents.join('\n\n'));
                     context.end();
-                });
+                })
+                .catch(error => winston.error(error));
         }
         else if (urlPath === '/jasmine.css') {
             fileReading = util.readFile(path.join(jasmineDir, 'jasmine.css'))
                 .then(content => {
                     context.css(content);
                     context.end();
-                });
+                })
+                .catch(error => winston.error(error));
         }
 
         return fileReading;
